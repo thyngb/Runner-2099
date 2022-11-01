2847e47dca5ce8c07dbdb9dc6526c8f580eb6d7b
Prystic <60816750+Prystic@users.noreply.github.com>
2022-11-01 21:32:14 +0800

    Updated Level Gen 4

--- Assets/Scripts/obstacleSample.cs
+++ Assets/Scripts/obstacleSample.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class obstacleSample : MonoBehaviour
@@ -11,52 +12,14 @@ public class obstacleSample : MonoBehaviour
     //D-Eon Vehicles
 
     //property
-    private Rigidbody2D rigidbody2d;
-    [SerializeField] Vector2 move;
-    [SerializeField] float speed;
 
-    //option
-    [SerializeField] bool option_physics;
-    [SerializeField] bool option_momentum;
-    [SerializeField] bool option_animation;
-
-    void Start()
-    {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        getOptioned();
-    }
-    private void getOptioned()
-    {
-        if (option_momentum == true) getPhysic();
-        if (option_momentum == true) getMomentum();
-        if (option_animation == true) getAnimation();
-    }
-
-    private void getAnimation()
-    {
-        throw new NotImplementedException();
-    }
-
-    private void getPhysic()
-    {
-        rigidbody2d.constraints = RigidbodyConstraints2D.FreezeRotation;
-    }
-
-    private void getMomentum()
-    {
-        move = gameObject.transform.position;
-        move.x -= speed; 
-        gameObject.transform.position = move;
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Contains("Player"))
         {
             Destroy(collision.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 }
